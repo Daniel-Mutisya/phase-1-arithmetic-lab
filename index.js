a5adf422fdca48128a868884b2c193ba6578cdde
daniel <danielmutisya203@gmail.com>
2023-03-15 02:58:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random); // Output: random integer greater than 0
+
+let num3 = 46;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod); // Output: 4
+
+let numbers = [5, 10, 15, 20];
+let max = Math.max(...numbers);
+console.log(max); // Output: 20
